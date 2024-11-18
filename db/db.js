3568308d44c91b49d4002ee419c774ff75d9fe15
1slom-media <islombektagayev1@gmail.com>
2024-11-19 03:22:06 +0500

    db prod replace

--- db/db.js
+++ db/db.js
@@ -1,20 +1,20 @@
 import Client from "pg";
 
-//connection
-// const client = new Client.Client({
-//   host: "localhost",
-//   user: "postgres",
-//   password: "Postgres",
-//   database: "allgood_bot",
-//   port: 5432,
-// });
-
+connection
 const client = new Client.Client({
   host: "localhost",
   user: "postgres",
-  password: "islom_01",
-  database: "allgood",
+  password: "Postgres",
+  database: "allgood_bot",
   port: 5432,
 });
+
+// const client = new Client.Client({
+//   host: "localhost",
+//   user: "postgres",
+//   password: "islom_01",
+//   database: "allgood",
+//   port: 5432,
+// });
 
 export default client;
